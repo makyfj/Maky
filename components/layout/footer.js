5bc8001e705e681e18f7bac057b23989071aaa18
Maky <franklinjaradev@gmail.com>
2021-09-09 00:24:35 -0400

    footer js

---
+++ components/layout/footer.js
@@ -0,0 +1,12 @@
+// components
+import classes from "./footer.module.scss";
+
+const Footer = () => {
+  return (
+    <footer className={classes.footer}>
+      <p>Copyright &copy; Franklin Jara</p>
+    </footer>
+  );
+};
+
+export default Footer;
--- components/layout/layout.js
+++ components/layout/layout.js
@@ -1,5 +1,4 @@
 import Navbar from "./navbar";
-
 const Layout = ({ children }) => {
   return (
     <>
