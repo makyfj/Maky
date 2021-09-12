f0efa918ef0d51cf1e2bd9d2ecd4e18675299f45
Maky <franklinjaradev@gmail.com>
2021-09-12 00:58:04 -0400

    Skills grid/component

--- components/home-page/skills.js
+++ components/home-page/skills.js
@@ -1,19 +1,71 @@
 import Image from "next/image";
 
-import { skills } from "../../data/skills";
+import classes from "./skills.module.scss";
+import {
+  frontEndSkills,
+  backEndSkills,
+  programmingSkills,
+  otherSkills,
+} from "../../data/skills";
 
 const Skills = () => {
   return (
-    <div>
-      <ul>
-        {skills.map((skill, index) => (
-          <li key={index}>
-            <h2>{skill.name}</h2>
-            <Image src={skill.icon} width={100} height={100} alt={skill.name} />
-          </li>
-        ))}
-      </ul>
-    </div>
+    <aside className={classes.container}>
+      <h1>Skills</h1>
+      <div className={classes.skills}>
+        <ul className={classes.front}>
+          <h3>Front End</h3>
+          {frontEndSkills.map((skill, index) => (
+            <li key={index}>
+              <h4>{skill.name}</h4>
+              <Image
+                src={skill.icon}
+                width={100}
+                height={100}
+                alt={skill.name}
+              />
+            </li>
+          ))}
+        </ul>
+        <ul className={classes.back}>
+          <h3>Back End</h3>
+          {backEndSkills.map((skill, index) => (
+            <li key={index}>
+              <h4>{skill.name}</h4>
+              <Image
+                src={skill.icon}
+                width={100}
+                height={100}
+                alt={skill.name}
+              />
+            </li>
+          ))}
+        </ul>
+
+        <ul className={classes.programming}>
+          <h3>Programming Languages</h3>
+          {programmingSkills.map((skill, index) => (
+            <li key={index}>
+              <h4>{skill.name}</h4>
+              <Image
+                src={skill.icon}
+                width={100}
+                height={100}
+                alt={skill.name}
+              />
+            </li>
+          ))}
+        </ul>
+        <ul className={classes.other}>
+          <h3>OtherSkills</h3>
+          {otherSkills.map((skill, index) => (
+            <li key={index}>
+              <h4>{skill.name}</h4>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </aside>
   );
 };
 
--- data/skills.js
+++ data/skills.js
@@ -1,7 +1,25 @@
+// front end
 import html5 from "devicon/icons/html5/html5-plain.svg";
 import css3 from "devicon/icons/css3/css3-plain.svg";
+import javascript from "devicon/icons/javascript/javascript-plain.svg";
+import react from "devicon/icons/react/react-original.svg";
+import redux from "devicon/icons/redux/redux-original.svg";
+import materialui from "devicon/icons/materialui/materialui-original.svg";
+import typescript from "devicon/icons/typescript/typescript-original.svg";
 
-export const skills = [
+// back end
+import mongodb from "devicon/icons/mongodb/mongodb-plain.svg";
+import express from "devicon/icons/express/express-original.svg";
+import nodejs from "devicon/icons/nodejs/nodejs-plain.svg";
+import mysql from "devicon/icons/mysql/mysql-plain.svg";
+import graphql from "devicon/icons/graphql/graphql-plain.svg";
+
+// programming language
+import python from "devicon/icons/python/python-original.svg";
+import cpp from "devicon/icons/cplusplus/cplusplus-original.svg";
+import java from "devicon/icons/java/java-original.svg";
+
+export const frontEndSkills = [
   // front end
   {
     name: "HTML5",
@@ -11,4 +29,70 @@ export const skills = [
     name: "CSS3",
     icon: css3,
   },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "React",
+    icon: react,
+  },
+  {
+    name: "Redux",
+    icon: redux,
+  },
+  { name: "Material-UI", icon: materialui },
+  { name: "TypeScript", icon: typescript },
+];
+
+export const backEndSkills = [
+  // back end
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "ExpressJs",
+    icon: express,
+  },
+  {
+    name: "NodeJS",
+    icon: nodejs,
+  },
+  {
+    name: "MySQL",
+    icon: mysql,
+  },
+  {
+    name: "GraphQL",
+    icon: graphql,
+  },
+];
+
+export const programmingSkills = [
+  // programming languages
+  {
+    name: "Python",
+    icon: python,
+  },
+  {
+    name: "C++",
+    icon: cpp,
+  },
+  {
+    name: "Java",
+    icon: java,
+  },
+];
+
+export const otherSkills = [
+  { name: "Data Structures and Algorithms" },
+  { name: "Object Oriented Programming" },
+  { name: "Software Design and Architectures" },
+  { name: "Agile" },
+  { name: "Git" },
+  { name: "NPM" },
+  { name: "Yarn" },
+  { name: "PyTorch" },
+  { name: "R" },
 ];
