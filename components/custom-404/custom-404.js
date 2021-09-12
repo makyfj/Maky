5dc4a7c30b17ca2512f54e3200040f5d19a72b69
Maky <franklinjaradev@gmail.com>
2021-09-12 15:48:34 -0400

    Custom 404

---
+++ components/custom-404/custom-404.js
@@ -0,0 +1,15 @@
+import Button from "../button/button";
+import classes from "./custom-404.module.scss";
+
+const Custom404 = () => {
+  return (
+    <>
+      <main className={classes.container}>
+        <h1>404 - Page Not Found</h1>
+        <Button href="/" name="Home" />
+      </main>
+    </>
+  );
+};
+
+export default Custom404;
---
+++ pages/404.js
@@ -0,0 +1,7 @@
+import Custom404 from "../components/custom-404/custom-404";
+
+const CustomNotFound = () => {
+  return <Custom404 />;
+};
+
+export default CustomNotFound;
