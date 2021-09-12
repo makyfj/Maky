c7ed7907c2f32cef3f84d51c05c31ec08f6b544f
Maky <franklinjaradev@gmail.com>
2021-09-12 13:26:23 -0400

    Skills component refactor

--- components/home-page/skills.js
+++ components/home-page/skills.js
@@ -7,48 +7,17 @@ import {
   programmingSkills,
   otherSkills,
 } from "../../data/skills";
+import Skill from "./skills/skill-item";
 
 const Skills = () => {
   return (
     <aside className={classes.container}>
       <h1>Skills</h1>
       <div className={classes.skills}>
-        <ul className={classes.front}>
-          <h3>Front End</h3>
-          {frontEndSkills.map((skill, index) => (
-            <li key={index}>
-              <h4>{skill.name}</h4>
-              <Image src={skill.icon} width={50} height={50} alt={skill.name} />
-            </li>
-          ))}
-        </ul>
-        <ul className={classes.back}>
-          <h3>Back End</h3>
-          {backEndSkills.map((skill, index) => (
-            <li key={index}>
-              <h4>{skill.name}</h4>
-              <Image src={skill.icon} width={50} height={50} alt={skill.name} />
-            </li>
-          ))}
-        </ul>
-
-        <ul className={classes.programming}>
-          <h3>Programming Languages</h3>
-          {programmingSkills.map((skill, index) => (
-            <li key={index}>
-              <h4>{skill.name}</h4>
-              <Image src={skill.icon} width={50} height={50} alt={skill.name} />
-            </li>
-          ))}
-        </ul>
-        <ul className={classes.other}>
-          <h3>OtherSkills</h3>
-          {otherSkills.map((skill, index) => (
-            <li key={index}>
-              <h4>{skill.name}</h4>
-            </li>
-          ))}
-        </ul>
+        <Skill skills={frontEndSkills} title="Front End" />
+        <Skill skills={backEndSkills} title="Back End" />
+        <Skill skills={programmingSkills} title="Programming Languages" />
+        <Skill skills={otherSkills} title="Other Skills" />
       </div>
     </aside>
   );
