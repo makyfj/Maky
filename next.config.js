8405de0f1ff45d1ca4a3be7bf00af1b826cff595
Maky <franklinjaradev@gmail.com>
2021-09-12 20:56:25 -0400

    Post typo and next-config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@ module.exports = {
   reactStrictMode: true,
   generateBuildId: async () => {
     // You can, for example, get the latest git commit hash here
-    return "ID";
+    return "build";
   },
 };
