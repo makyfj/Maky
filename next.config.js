a0a6edfbb968a2206a5c6cb7ae91938e98e04289
Maky <franklinjaradev@gmail.com>
2021-10-14 20:45:05 -0400

    chore: adding next-images

--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
 const path = require("path");
-const withImages = require("next-images");
 
-module.exports = withImages({
+module.exports = {
   reactStrictMode: true,
   sassOptions: {
     includePaths: [path.join(__dirname, "styles/*")],
   },
-});
+};
