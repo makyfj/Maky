60532158651fb6112e71e695c59f81a54ed4517e
Maky <franklinjaradev@gmail.com>
2021-10-15 20:43:32 -0400

    chore: next-mdx-remote upgrade

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
-const path = require("path");
-
 module.exports = {
   reactStrictMode: true,
-  sassOptions: {
-    includePaths: [path.join(__dirname, "styles/*")],
+  generateBuildId: async () => {
+    // You can, for example, get the latest git commit hash here
+    return "build";
   },
 };
