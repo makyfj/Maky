6dbd799f2b05f39d23a3353c6707fa632174710c
Maky <franklinjaradev@gmail.com>
2021-12-18 18:41:56 -0500

    chore: adding resume icon and file

--- data/socials.js
+++ data/socials.js
@@ -1,5 +1,6 @@
 import GitHubIcon from "@material-ui/icons/GitHub";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import AttachmentIcon from "@material-ui/icons/Attachment";
 
 export const socials = [
   {
@@ -11,5 +12,10 @@ export const socials = [
     id: 2,
     link: "https://linkedin.com/in/franklin-jara-fj",
     icon: <LinkedInIcon fontSize="large" />,
+  },
+  {
+    id: 3,
+    link: "file/FranklinJaraResume.pdf",
+    icon: <AttachmentIcon fontSize="large" />,
   },
 ];
